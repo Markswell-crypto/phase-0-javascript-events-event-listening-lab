e79017896981cc32453dcbe38dc8d991589317b4
Markswell <osoromarkswell80@gmail.com>
2023-09-15 16:26:44 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+  let button = document.getElementById('button');
+
+  button.addEventListener('click', function() {
+    // Your event handler logic here
+  });
 }
